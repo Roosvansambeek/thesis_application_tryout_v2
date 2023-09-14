915143c80c6ef56e043a55a5d47f02c130e915a3
Roosvansambeek <123948541+Roosvansambeek@users.noreply.github.com>
2023-09-14 10:55:06 +0000

    latest changes, not able to make an applicaiton work

--- database.py
+++ database.py
@@ -34,9 +34,16 @@ def load_job_from_db(id):
 
 def add_application_to_db(job_id, data):
     with engine.connect() as conn:
-        query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url) VALUES (:job_id, :full_name, :email, :linkedin_url)")
+        query = text("INSERT INTO applications2 (full_name, email) VALUES (:full_name, :email)")
+      
         conn.execute(query,
-                     job_id=job_id,
-                     full_name=data['full_name'],  # Corrected variable name here
-                     email=data['email'],  # Corrected variable name here
-                     linkedin_url=data['linkedin_url'])  # Corrected variable name here
+                    #job_id=job_id,
+                    full_name =data['full_name'],
+                    email =data['email'])
+                    #education =data['education'],
+                    #work_experience =data['work_experience'],
+                    #resume_url = data['resume_url'])
+
+
+
+
