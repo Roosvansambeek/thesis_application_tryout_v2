e382896d8d61d06f37dce6a37221d2b1b9041e3d
Roosvansambeek <123948541+Roosvansambeek@users.noreply.github.com>
2023-09-17 20:07:32 +0000

    fiiiiinaly sending to sql

--- app.py
+++ app.py
@@ -6,13 +6,10 @@ import json
 app = Flask(__name__)
 
 
-
-
 @app.route("/")
 def hello_world():
   jobs = load_jobs_from_db()
-  return  render_template('home.html', 
-                          jobs=jobs)
+  return  render_template('home.html', jobs=jobs)
   
 
 @app.route("/api/jobs")
@@ -27,18 +24,18 @@ def show_job(id):
   if not job:
     return "Not Found", 404
   
-  return render_template('jobpage.html', 
-                         job=job)
+  return render_template('jobpage.html',job=job)
 
 @app.route("/job/<id>/apply", methods=['post'])
 def apply_to_job(id):
-  data = request.form
-  job = load_job_from_db(id)
-  add_application_to_db(id, data)
-  return render_template('application_submitted.html',
-                        application=data,
-                        job=job)
-  
+    data = request.form
+    job = load_job_from_db(id)  # Load the job associated with the given ID
+    
+    # Add the application data to the database
+    add_application_to_db(id, data)
+    
+    return render_template('application_submitted.html', job=job, application=data)
+
   
   
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -32,17 +32,22 @@ def load_job_from_db(id):
         else:
             return row._asdict()  # Convert the single row to a dictionary
 
+
 def add_application_to_db(job_id, data):
     with engine.connect() as conn:
-        query = text("INSERT INTO applications2 (full_name, email) VALUES (:full_name, :email)")
-      
-        conn.execute(query,
-                    #job_id=job_id,
-                    full_name =data['full_name'],
-                    email =data['email'])
-                    #education =data['education'],
-                    #work_experience =data['work_experience'],
-                    #resume_url = data['resume_url'])
+        query = text("INSERT INTO applications2 (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+        
+        # Bind the parameters to the query
+        query = query.bindparams(job_id=job_id,full_name=data['full_name'],
+                                 email=data['email'],
+                                 linkedin_url=data['linkedin_url'],
+                                 education=data['education'],
+                                 work_experience=data['work_experience'],
+                                 resume_url=data['resume_url'])
+
+        conn.execute(query)
+
+
 
 
 
